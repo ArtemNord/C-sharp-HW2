07c38649ad989fb7223996e11c4829e0dc6efe8c
Naganishe <mr.nordakov@gmail.com>
2022-06-11 12:14:45 +0300

    add C# HW 1

---
+++ C# HW 1/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу которая на входе принимает трехзначное число
+// и на выходе показывает вторую цифру этого числа.
+Console.Write ("Введите число: ");
+int number = Convert.ToInt32 (Console.ReadLine());
+
+int result = (number % 100) / 10;
+
+Console.Write ("Второе число: "); 
+Console.Write (result);
